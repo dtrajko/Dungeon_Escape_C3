5d639b76838b481045f78673071f48f89561929d
Dejan Trajkovic <dtrajko@gmail.com>
2019-08-30 03:51:34 +0200

    Section 5: Player Setup - 34. Challenge Review - Player Jumping P.1

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,10 @@ public class Player : MonoBehaviour
 {
     // Get reference to Rigidbody
     private Rigidbody2D _rigid;
+    [SerializeField] private float _jumpForce = 5.0f;
+    [SerializeField] private float _moveSpeed = 2.0f;
+    [SerializeField] private bool _grounded = false;
+    [SerializeField] private LayerMask _groundLayer;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +24,20 @@ public class Player : MonoBehaviour
     {
         // Horizontal input for left / right
         float move = CrossPlatformInputManager.GetAxisRaw("Horizontal");
-        _rigid.velocity = new Vector2(move, _rigid.velocity.y);
+
+        if (CrossPlatformInputManager.GetButtonDown("Jump") && _grounded) {
+            _rigid.velocity = new Vector2(_rigid.velocity.x, _jumpForce);
+            _grounded = false;
+        }
+
+        // 2D raycast to the ground
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down, 0.6f, _groundLayer.value);
+        Debug.DrawRay(transform.position, Vector2.down, Color.green);
+
+        if (hitInfo.collider != null)
+        {
+            _grounded = true;
+        }
+        _rigid.velocity = new Vector2(move * _moveSpeed, _rigid.velocity.y);
     }
 }
