5e28e84baad869d0f5c14375d9d4483ccc41acd5
Dejan Trajkovic <dtrajko@gmail.com>
2019-08-31 07:41:20 +0200

    Section 5: Player Setup - 36. Player Jump Optimized (I)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -24,26 +24,37 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Movement();
+        CheckGrounded();
+    }
+
+    void Movement() {
         // Horizontal input for left / right
         float move = CrossPlatformInputManager.GetAxisRaw("Horizontal");
 
-        if (CrossPlatformInputManager.GetButtonDown("Jump") && _grounded) {
+        if (CrossPlatformInputManager.GetButtonDown("Jump") && _grounded)
+        {
             _rigid.velocity = new Vector2(_rigid.velocity.x, _jumpForce);
             _grounded = false;
             resetJumpNeeded = true;
             StartCoroutine(ResetJumpNeededCoroutine());
         }
 
+        _rigid.velocity = new Vector2(move * _moveSpeed, _rigid.velocity.y);
+    }
+
+    void CheckGrounded() {
         // 2D raycast to the ground
         RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down, 0.6f, _groundLayer.value);
         Debug.DrawRay(transform.position, Vector2.down, Color.green);
 
-        if (hitInfo.collider != null) {
-            if (resetJumpNeeded == false) {
+        if (hitInfo.collider != null)
+        {
+            if (resetJumpNeeded == false)
+            {
                 _grounded = true;
             }
         }
-        _rigid.velocity = new Vector2(move * _moveSpeed, _rigid.velocity.y);
     }
 
     IEnumerator ResetJumpNeededCoroutine() {
