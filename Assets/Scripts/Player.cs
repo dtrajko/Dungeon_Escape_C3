e3f42f33fb9d8d1df316b07929dc6928224aaae1
Dejan Trajkovic <dtrajko@gmail.com>
2019-08-31 10:10:23 +0200

    Section 5: Player Setup - 42. Challenge Review - Player Run Animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,12 +13,16 @@ public class Player : MonoBehaviour
 
     private Rigidbody2D _rigid;
     private bool _resetJump = false;
+    private PlayerAnimation _playerAnimation;
+    private float localScaleX;
 
     // Start is called before the first frame update
     void Start()
     {
         // Assign handle of Rigidbody
         _rigid = GetComponent<Rigidbody2D>();
+        _playerAnimation = GetComponent<PlayerAnimation>();
+        localScaleX = transform.localScale.x;
     }
 
     // Update is called once per frame
@@ -30,16 +34,22 @@ public class Player : MonoBehaviour
     void Movement() {
         // Horizontal input for left / right
         float move = CrossPlatformInputManager.GetAxisRaw("Horizontal");
-        _rigid.velocity = new Vector2(move * _speed, _rigid.velocity.y);
 
         if (Input.GetKeyDown(KeyCode.Space) && IsGrounded()) {
             _rigid.velocity = new Vector2(_rigid.velocity.x, _jumpForce);
             StartCoroutine(ResetJumpNeededCoroutine());
         }
+
+        if (move < -0.1f) localScaleX = -1;
+        if (move > 0.1f) localScaleX = 1;
+        transform.localScale = new Vector3(localScaleX, transform.localScale.y, transform.localScale.z);
+
+        _rigid.velocity = new Vector2(move * _speed, _rigid.velocity.y);
+        _playerAnimation.Move(move);
     }
 
     bool IsGrounded() {
-        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down, 0.6f, _groundLayer.value);
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down, 0.8f, _groundLayer.value);
         if (hitInfo.collider != null) {
             if (_resetJump == false) {
                 return true;
---
+++ Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    // Handle to Animator
+    private Animator _animator;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Assign Handle to Animator
+        _animator = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Move(float move) {
+        _animator.SetFloat("Move", Mathf.Abs(move));
+    }
+}
