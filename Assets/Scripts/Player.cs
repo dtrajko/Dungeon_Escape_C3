4ea8beb75828646d2d00337fa06739a26c384ead
Dejan Trajkovic <dtrajko@gmail.com>
2019-08-31 07:37:16 +0200

    Section 5: Player Setup - 35. Challenge Review - Player Jumping P.2

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,11 +6,13 @@ using UnityStandardAssets.CrossPlatformInput;
 public class Player : MonoBehaviour
 {
     // Get reference to Rigidbody
-    private Rigidbody2D _rigid;
     [SerializeField] private float _jumpForce = 5.0f;
     [SerializeField] private float _moveSpeed = 2.0f;
     [SerializeField] private bool _grounded = false;
     [SerializeField] private LayerMask _groundLayer;
+
+    private Rigidbody2D _rigid;
+    private bool resetJumpNeeded = false;
 
     // Start is called before the first frame update
     void Start()
@@ -28,16 +30,24 @@ public class Player : MonoBehaviour
         if (CrossPlatformInputManager.GetButtonDown("Jump") && _grounded) {
             _rigid.velocity = new Vector2(_rigid.velocity.x, _jumpForce);
             _grounded = false;
+            resetJumpNeeded = true;
+            StartCoroutine(ResetJumpNeededCoroutine());
         }
 
         // 2D raycast to the ground
         RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down, 0.6f, _groundLayer.value);
         Debug.DrawRay(transform.position, Vector2.down, Color.green);
 
-        if (hitInfo.collider != null)
-        {
-            _grounded = true;
+        if (hitInfo.collider != null) {
+            if (resetJumpNeeded == false) {
+                _grounded = true;
+            }
         }
         _rigid.velocity = new Vector2(move * _moveSpeed, _rigid.velocity.y);
+    }
+
+    IEnumerator ResetJumpNeededCoroutine() {
+        yield return new WaitForSeconds(0.1f);
+        resetJumpNeeded = false;
     }
 }
