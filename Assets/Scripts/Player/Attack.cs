5f08f44a26e7cb7a049d2e6e7521b57514dc9dd0
Dejan Trajkovic <dtrajko@gmail.com>
2019-09-11 03:04:54 +0200

    Section 7: Attack System - 92. Player Attack Hitbox Setup

---
+++ Assets/Scripts/Player/Attack.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other) {
+
+    }
+}
