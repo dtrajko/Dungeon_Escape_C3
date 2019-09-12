8fb9bcd43695b1399fd2b2146632cc9a3fa9d46a
Dejan Trajkovic <dtrajko@gmail.com>
2019-09-12 04:45:30 +0200

    Section 7: Attack System - 102. Skeleton Damage Cooldown

--- Assets/Scripts/Player/Attack.cs
+++ Assets/Scripts/Player/Attack.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class Attack : MonoBehaviour
 {
+    private bool _canDamage = true;
+
     private void OnTriggerEnter2D(Collider2D other) {
         Debug.Log("Attacking " + other.name);
 
         IDamageable hit = other.GetComponent<IDamageable>();
 
-        if (hit != null) {
+        if (hit != null && _canDamage == true) {
             hit.Damage();
+            _canDamage = false;
+            StartCoroutine(ResetDamage());
         }
+    }
+
+    IEnumerator ResetDamage() {
+        yield return new WaitForSeconds(0.5f);
+        _canDamage = true;
     }
 }
