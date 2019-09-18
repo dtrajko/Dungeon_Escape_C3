bb792f382ca3c2a52c49298ef06648970eb23be8
Dejan Trajkovic <dtrajko@gmail.com>
2019-09-18 03:47:22 +0200

    Section 7: Attack System - 120. Spider Animation Event

---
+++ Assets/Scripts/Enemy/SpiderAnimationEvent.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderAnimationEvent : MonoBehaviour
+{
+    public void Fire() {
+        Debug.Log("Spider should Fire!");
+    }
+}
