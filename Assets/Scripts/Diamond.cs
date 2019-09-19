1f19c6fac5ec40fc001190b5f03667c641fe1871
Dejan Trajkovic <dtrajko@gmail.com>
2019-09-19 06:44:16 +0200

    Section 8: Loot System - 126. Diamond Behaviour

---
+++ Assets/Scripts/Diamond.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Diamond : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+    }
+}
