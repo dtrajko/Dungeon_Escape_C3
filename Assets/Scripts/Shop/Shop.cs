21be2f18ed832ae644353199c789f7b825d6d734
Dejan Trajkovic <dtrajko@gmail.com>
2019-09-22 03:24:47 +0200

    Section 8: Loot System - 134. Enable Shop

---
+++ Assets/Scripts/Shop/Shop.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    [SerializeField] private GameObject shopPanel;
+
+    private void Start() {
+        shopPanel.SetActive(false);
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Player") {
+            shopPanel.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            shopPanel.SetActive(false);
+        }
+    }
+}
