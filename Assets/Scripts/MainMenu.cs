713724f2fda025195838a0fe231b5f1311741d40
Dejan Trajkovic <dtrajko@gmail.com>
2019-09-26 03:27:00 +0200

    Section 10: Main Menu - 161. Main Menu Functionality

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartButton()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+
+    public void MenuButton()
+    {
+        Debug.Log("MenuButton pressed! (TODO)");
+    }
+}
